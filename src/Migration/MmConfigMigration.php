8be2bcf23ea1b2475437a422a48df14893f5545a
Bob <bob@brightcloudstudio.com>
2026-05-28 08:00:36 -0400

    Align mmenu config migration schema

--- src/Migration/MmConfigMigration.php
+++ src/Migration/MmConfigMigration.php
@@ -74,29 +74,16 @@ final class MmConfigMigration extends AbstractMigration
                     id INT UNSIGNED AUTO_INCREMENT NOT NULL,
                     tstamp INT UNSIGNED DEFAULT 0 NOT NULL,
                     title VARCHAR(255) DEFAULT '' NOT NULL,
+                    navbarTitle VARCHAR(255) DEFAULT '' NOT NULL,
                     position VARCHAR(32) DEFAULT '' NOT NULL,
                     zposition VARCHAR(32) DEFAULT '' NOT NULL,
                     slidingSubmenus VARCHAR(32) DEFAULT '' NOT NULL,
                     theme VARCHAR(32) DEFAULT '' NOT NULL,
-                    moveBackground TINYINT(1) DEFAULT 1 NOT NULL,
-                    fullscreen TINYINT(1) DEFAULT 0 NOT NULL,
+                    themeHighContrast TINYINT(1) DEFAULT 0 NOT NULL,
                     countersAdd TINYINT(1) DEFAULT 0 NOT NULL,
-                    columnsAdd TINYINT(1) DEFAULT 0 NOT NULL,
                     searchfieldAdd TINYINT(1) DEFAULT 0 NOT NULL,
-                    pageDim VARCHAR(16) DEFAULT '' NOT NULL,
-                    menuEffects VARCHAR(16) DEFAULT '' NOT NULL,
-                    panelEffects VARCHAR(16) DEFAULT '' NOT NULL,
-                    listEffects VARCHAR(16) DEFAULT '' NOT NULL,
-                    dragOpenEnable TINYINT(1) DEFAULT 0 NOT NULL,
-                    dragOpenThreshold SMALLINT DEFAULT 50 NOT NULL,
-                    dragOpenMaxStartPos SMALLINT DEFAULT 100 NOT NULL,
-                    polyfillEnable TINYINT(1) DEFAULT 0 NOT NULL,
-                    onClickClose TINYINT(1) DEFAULT 0 NOT NULL,
                     pageSelector VARCHAR(255) DEFAULT '' NOT NULL,
                     iconPanels TINYINT(1) DEFAULT 0 NOT NULL,
-                    shadows BLOB DEFAULT NULL,
-                    keyboardNavigation TINYINT(1) DEFAULT 0 NOT NULL,
-                    keyboardNavigationEnhance TINYINT(1) DEFAULT 0 NOT NULL,
                 PRIMARY KEY(id))",
             );
         }
@@ -121,6 +108,7 @@ final class MmConfigMigration extends AbstractMigration
 
         if ($schemaManager->tablesExist(['tl_dk_mmenu_config', 'tl_module'])) {
             $columns = $schemaManager->listTableColumns('tl_module');
+            $configColumns = $schemaManager->listTableColumns('tl_dk_mmenu_config');
 
             foreach (self::LEGACY_COLUMNS as $column) {
                 if (!isset($columns[$column])) {
@@ -137,29 +125,22 @@ final class MmConfigMigration extends AbstractMigration
                 $config = [];
                 $config['title'] = $module['name'];
                 $config['tstamp'] = time();
+                $config['navbarTitle'] = '';
                 $config['position'] = $module['dk_mmenuPosition'];
                 $config['zposition'] = $module['dk_mmenuZposition'];
                 $config['slidingSubmenus'] = $module['dk_mmenuSlidingSubmenus'];
                 $config['theme'] = $module['dk_mmenuTheme'];
-                $config['moveBackground'] = (int) ($module['dk_mmenuMoveBackground'] ?? 1);
-                $config['pageDim'] = $module['dk_mmenuPageDim'] ?? '';
-                $config['fullscreen'] = (int) ($module['dk_mmenuFullscreen'] ?? 0);
+                $config['themeHighContrast'] = (int) ($module['dk_mmenuThemeHighContrast'] ?? 0);
                 $config['countersAdd'] = (int) ($module['dk_mmenuCountersAdd'] ?? 0);
-                $config['columnsAdd'] = (int) ($module['dk_mmenuColumnsAdd'] ?? 0);
                 $config['searchfieldAdd'] = (int) ($module['dk_mmenuSearchfieldAdd'] ?? 0);
                 $config['iconPanels'] = (int) ($module['dk_mmenuIconPanels'] ?? 0);
-                $config['menuEffects'] = $module['dk_mmenuMenuEffects'] ?? '';
-                $config['panelEffects'] = $module['dk_mmenuPanelEffects'] ?? '';
-                $config['listEffects'] = $module['dk_mmenuListEffects'] ?? '';
-                $config['shadows'] = (int) ($module['dk_mmenuShadows'] ?? 0);
-                $config['onClickClose'] = (int) ($module['dk_mmenuOnClickClose'] ?? 0);
                 $config['pageSelector'] = $module['dk_mmenuPageSelector'] ?? '';
-                $config['dragOpenEnable'] = (int) ($module['dk_mmenuDragOpenEnable'] ?? 0);
-                $config['dragOpenMaxStartPos'] = (int) ($module['dk_mmenuDragOpenMaxStartPos'] ?? 0);
-                $config['dragOpenThreshold'] = (int) ($module['dk_mmenuDragOpenThreshold'] ?? 50);
-                $config['polyfillEnable'] = (int) ($module['dk_mmenuPolyfillEnable'] ?? 0);
-                $config['keyboardNavigation'] = (int) ($module['dk_mmenuKeyboardNavigation'] ?? 0);
-                $config['keyboardNavigationEnhance'] = (int) ($module['dk_mmenuKeyboardNavigationEnhance'] ?? 0);
+
+                $config = array_filter(
+                    $config,
+                    static fn (string $column): bool => isset($configColumns[strtolower($column)]),
+                    ARRAY_FILTER_USE_KEY,
+                );
 
                 $this->connection->insert('tl_dk_mmenu_config', $config);
                 $configId = (int) $this->connection->lastInsertId();
